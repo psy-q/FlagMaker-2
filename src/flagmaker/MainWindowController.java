32be9c31d03dc7aada2821843aed5e3f414d5f9a
Andrew Arnold <andrew.s.arnold@gmail.com>
2015-10-01 13:39:11 -0400

    More toolbar ratio stuff; removing dist folder

--- src/flagmaker/MainWindowController.java
+++ src/flagmaker/MainWindowController.java
@@ -1,16 +1,56 @@
 package flagmaker;
 
 import javafx.fxml.FXML;
-import javax.swing.JOptionPane;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 public class MainWindowController
 {
-    @FXML private TextField txtRatioHeight;
+    @FXML
+    private TextField txtRatioHeight;
+    @FXML
+    private TextField txtRatioWidth;
+    @FXML
+    private ComboBox cmbRatio;
 
     @FXML
-    public void SetRatio()
+    protected void initialize()
     {
-	JOptionPane.showMessageDialog(null, txtRatioHeight.getText(), "FlagMaker", JOptionPane.INFORMATION_MESSAGE);
+	RatioTextboxChanged();
+	cmbRatio.getSelectionModel().select(0);
+    }
+    
+    @FXML
+    public void RatioTextboxChanged()
+    {
+	String sheight = txtRatioHeight.getText();
+	String swidth = txtRatioWidth.getText();	
+	cmbRatio.getItems().clear();
+	
+	int height = CanParseInt(sheight)
+		? Integer.parseInt(sheight)
+		: 1;
+	int width = CanParseInt(swidth)
+		? Integer.parseInt(swidth)
+		: 1;
+
+	for (int i = 1; i <= 20; i++)
+	{
+	    int h = i * height;
+	    int w = i * width;
+	    cmbRatio.getItems().add(h + ":" + w);
+	}
+    }
+
+    private boolean CanParseInt(String value)
+    {
+	try
+	{
+	    Integer.parseInt(value);
+	    return true;
+	}
+	catch (NumberFormatException e)
+	{
+	    return false;
+	}
     }
 }
