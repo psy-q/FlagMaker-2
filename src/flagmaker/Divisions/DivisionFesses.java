27d057291c1f43256aaac31d7213d60817033f6b
Andrew Arnold <andrew.s.arnold@gmail.com>
2015-10-06 14:22:05 -0400

    SVG and draw implementations for DivisionFesses and DivisionPales

--- src/flagmaker/Divisions/DivisionFesses.java
+++ src/flagmaker/Divisions/DivisionFesses.java
@@ -1,5 +1,6 @@
 package flagmaker.Divisions;
 
+import flagmaker.ColorExtensions;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -22,9 +23,14 @@ public class DivisionFesses extends Division
 	{
 		double height = canvas.getHeight();
 		double width = canvas.getWidth();
+		
+		double sizeSum = Values[0] + Values[1] + Values[2];
+		
+		double r1Size = height * Values[0] / sizeSum;
+		double r2Size = height * (Values[0] + Values[1]) / sizeSum;
 				
-		Rectangle top = new Rectangle(width, height / 3.0, Colors[0]);
-		Rectangle middle = new Rectangle(width, height * 2.0 / 3.0, Colors[1]);
+		Rectangle top = new Rectangle(width, r1Size, Colors[0]);
+		Rectangle middle = new Rectangle(width, r2Size, Colors[1]);
 		Rectangle bottom = new Rectangle(width, height, Colors[2]);
 		canvas.getChildren().addAll(bottom, middle, top);
 	}
@@ -32,16 +38,46 @@ public class DivisionFesses extends Division
 	@Override
 	public void SetColors(Color[] colors)
 	{
+		Colors[0] = colors[0];
+		Colors[1] = colors[1];
+		Colors[2] = colors[2];
 	}
 	
 	@Override
 	public void SetValues(int[] values)
 	{
+		Values[0] = values[0];
+		Values[1] = values[1];
+		Values[2] = values[2];
 	}
 	
 	@Override
 	public String ExportSvg(int width, int height)
 	{
-		return "";
+		StringBuilder sb = new StringBuilder();
+
+		double sizeSum = Values[0] + Values[1] + Values[2];
+		double r1Size = height * Values[0] / sizeSum;
+		double r2Size = height * (Values[0] + Values[1]) / sizeSum;
+			
+		// Bottom
+		sb.append(String.format("<rect width=\"%d\" height=\"%d\" x=\"0\" y=\"0\" %s />",
+				width,
+				height,
+				ColorExtensions.ToSvgFillWithOpacity(Colors[2])));
+		
+		// Middle
+		sb.append(String.format("<rect width=\"%d\" height=\"%.3f\" x=\"0\" y=\"0\" %s />",
+				width,
+				r2Size,
+				ColorExtensions.ToSvgFillWithOpacity(Colors[1])));
+		
+		// Top
+		sb.append(String.format("<rect width=\"%d\" height=\"%.3f\" x=\"0\" y=\"0\" %s />",
+				width,
+				r1Size,
+				ColorExtensions.ToSvgFillWithOpacity(Colors[0])));
+		
+		return sb.toString();
 	}
 }
--- src/flagmaker/Divisions/DivisionPales.java
+++ src/flagmaker/Divisions/DivisionPales.java
@@ -1,5 +1,6 @@
 package flagmaker.Divisions;
 
+import flagmaker.ColorExtensions;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -22,9 +23,14 @@ public class DivisionPales extends Division
 	{
 		double height = canvas.getHeight();
 		double width = canvas.getWidth();
+		
+		double sizeSum = Values[0] + Values[1] + Values[2];
+		
+		double r1Size = width * Values[0] / sizeSum;
+		double r2Size = width * (Values[0] + Values[1]) / sizeSum;
 				
-		Rectangle left = new Rectangle(width / 3.0, height, Colors[0]);
-		Rectangle center = new Rectangle(width * 2.0 / 3.0, height, Colors[1]);
+		Rectangle left = new Rectangle(r1Size, height, Colors[0]);
+		Rectangle center = new Rectangle(r2Size, height, Colors[1]);
 		Rectangle right = new Rectangle(width, height, Colors[2]);
 		canvas.getChildren().addAll(right, center, left);
 	}
@@ -32,16 +38,46 @@ public class DivisionPales extends Division
 	@Override
 	public void SetColors(Color[] colors)
 	{
+		Colors[0] = colors[0];
+		Colors[1] = colors[1];
+		Colors[2] = colors[2];
 	}
 	
 	@Override
 	public void SetValues(int[] values)
 	{
+		Values[0] = values[0];
+		Values[1] = values[1];
+		Values[2] = values[2];
 	}
 	
 	@Override
 	public String ExportSvg(int width, int height)
 	{
-		return "";
+		StringBuilder sb = new StringBuilder();
+
+		double sizeSum = Values[0] + Values[1] + Values[2];
+		double r1Size = width * Values[0] / sizeSum;
+		double r2Size = width * (Values[0] + Values[1]) / sizeSum;
+			
+		// Bottom
+		sb.append(String.format("<rect width=\"%d\" height=\"%d\" x=\"0\" y=\"0\" %s />",
+				width,
+				height,
+				ColorExtensions.ToSvgFillWithOpacity(Colors[2])));
+		
+		// Middle
+		sb.append(String.format("<rect width=\"%.3f\" height=\"%d\" x=\"0\" y=\"0\" %s />",
+				r2Size,
+				height,
+				ColorExtensions.ToSvgFillWithOpacity(Colors[1])));
+		
+		// Top
+		sb.append(String.format("<rect width=\"%.3f\" height=\"%d\" x=\"0\" y=\"0\" %s />",
+				r1Size,
+				height,
+				ColorExtensions.ToSvgFillWithOpacity(Colors[0])));
+		
+		return sb.toString();
 	}
 }
