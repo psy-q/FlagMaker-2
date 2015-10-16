48c25e3c359430d92fee2bb28d3ba2a4abd978d9
andrewsarnold <andrew.s.arnold@gmail.com>
2015-10-16 14:37:07 -0400

    OverlayFlag

--- src/flagmaker/Overlays/OverlayTypes/ShapeTypes/OverlayFlag.java
+++ src/flagmaker/Overlays/OverlayTypes/ShapeTypes/OverlayFlag.java
@@ -3,8 +3,11 @@ package flagmaker.Overlays.OverlayTypes.ShapeTypes;
 import flagmaker.Divisions.DivisionGrid;
 import flagmaker.Flag;
 import flagmaker.Overlays.Overlay;
+import flagmaker.Overlays.OverlayTypes.RepeaterTypes.OverlayRepeater;
 import flagmaker.Ratio;
 import java.io.File;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
@@ -60,13 +63,46 @@ public class OverlayFlag extends OverlayShape
 
 	@Override
 	public void Draw(Pane canvas)
-	{
+	{		
+		double canvasWidth = canvas.getWidth() * GetAttribute("Width").Value / MaximumX;
+		double canvasHeight = canvas.getHeight() * GetAttribute("Height").Value / MaximumY;
 		
+		AnchorPane a = new AnchorPane();
+		Scene s = new Scene(a, canvasWidth, canvasHeight);
+		Rectangle clip = new Rectangle(canvasWidth, canvasHeight);
+		Pane p = new Pane();
+		p.setClip(clip);
+		s.setRoot(p);
+		Flag.Draw(p);
+		
+		p.setLayoutX(canvas.getWidth() * GetAttribute("X").Value / MaximumX);
+		p.setLayoutY(canvas.getHeight() * GetAttribute("Y").Value / MaximumX);
+		canvas.getChildren().add(p);
 	}
 
 	@Override
 	public String ExportSvg(int width, int height)
 	{
-		return "";
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(String.format("<g transform=\"translate(%.3f,%.3f) scale(%.3f %.3f)\">\n",
+			width * (GetAttribute("X").Value / MaximumX),
+			height * (GetAttribute("Y").Value / MaximumY),
+			GetAttribute("Width").Value / MaximumX,
+			GetAttribute("Height").Value / MaximumY));
+
+		sb.append(Flag.Division.ExportSvg(width, height));
+
+		for (int i = 0; i < Flag.Overlays.length; i++)
+		{
+			if (i == 0 || !(Flag.Overlays[i - 1] instanceof OverlayRepeater))
+			{
+				sb.append(Flag.Overlays[i].ExportSvg(width, height));
+			}
+		}
+
+		sb.append("</g>\n");
+
+		return sb.toString();
 	}
 }
