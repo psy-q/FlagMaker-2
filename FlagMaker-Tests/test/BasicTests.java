bae7204db211895334339460a61e60681980fecf
Andrew Arnold <andrew.s.arnold@gmail.com>
2015-10-28 10:19:11 -0400

    Creating test project - moving all files down into a separate folder

---
+++ FlagMaker-Tests/test/BasicTests.java
@@ -0,0 +1,39 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BasicTests
+{
+	public BasicTests()
+	{
+	}
+	
+	@BeforeClass
+	public static void setUpClass()
+	{
+	}
+	
+	@AfterClass
+	public static void tearDownClass()
+	{
+	}
+	
+	@Before
+	public void setUp()
+	{
+	}
+	
+	@After
+	public void tearDown()
+	{
+	}
+
+	@Test
+	public void BasicTest()
+	{
+		assertTrue(1 == 1);
+	}
+}
